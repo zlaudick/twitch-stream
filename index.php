f708ec2cc44a04aed006c095c7c5801be5058e34
Zac Laudick <zlaudick@nmsu.edu>
2016-07-15 11:31:20 -0600

    added content to Persona, Use Case, and Interaction Flow

--- index.php
+++ index.php
@@ -6,8 +6,16 @@
 	</head>
 	<body>
 		<h2>Persona</h2>
+			<p>Zac Laudick is a gamer and he enjoys watching other people stream gameplay.</p>
 		<h2>Use Case</h2>
+			<p>Zac is looking for a streamer playing one of his favorite games.</p>
 		<h2>Interaction Flow</h2>
+			<ol>
+				<li>Go to twitch.tv homepage</li>
+				<li>Click browse</li>
+				<li>Find desired game and click the link</li>
+				<li>Click on one of the current streamers to watch</li>
+			</ol>
 		<h2>Conceptual Model</h2>
 	</body>
 </html>
