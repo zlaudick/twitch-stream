0fb68929f346811935c1fcecf51d0668c322f601
Zac Laudick <zlaudick@nmsu.edu>
2016-07-15 11:20:21 -0600

    added title and heading tags

--- index.php
+++ index.php
@@ -2,6 +2,12 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8"/>
+		<title>Data Design Phase 1</title>
 	</head>
-	<body></body>
+	<body>
+		<h2>Persona</h2>
+		<h2>Use Case</h2>
+		<h2>Interaction Flow</h2>
+		<h2>Conceptual Model</h2>
+	</body>
 </html>
