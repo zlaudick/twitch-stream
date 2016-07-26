6ba5c29f9f40d59288713ca1b1d4d7d19a5bc598
Zac Laudick <zlaudick@nmsu.edu>
2016-07-26 09:40:12 -0600

    added userPasswordHash and userPasswordSalt to twitch.sql

--- sql/twitch.sql
+++ sql/twitch.sql
@@ -9,6 +9,8 @@ CREATE TABLE user (
 	userEmail VARCHAR(128) NOT NULL,
 	userImage VARCHAR(128) NOT NULL,
 	userName VARCHAR(32) NOT NULL,
+	userPasswordHash NVARCHAR(128) NOT NULL,
+	userPasswordSalt NVARCHAR(128) NOT NULL,
 	UNIQUE(userEmail),
 	UNIQUE(userName),
 	PRIMARY KEY (userId)
